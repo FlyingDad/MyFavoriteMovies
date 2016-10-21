4c186c92cce163913dbad621bd40fdd91514b861
FlyingDad <Michael@michaels-imac.attlocal.net>
2016-10-21 16:13:24 -0500

    User able to favorite/unfavorite movie

--- MovieDetailViewController.swift
+++ MovieDetailViewController.swift
@@ -172,22 +172,93 @@ class MovieDetailViewController: UIViewController {
     
     @IBAction func toggleFavorite(sender: AnyObject) {
         
-        // let shouldFavorite = !isFavorite
+        let shouldFavorite = !isFavorite
         
         /* TASK: Add movie as favorite, then update favorite buttons */
         /* 1. Set the parameters */
+        let methodParameters: [String: String!] = [
+            Constants.TMDBParameterKeys.ApiKey : Constants.TMDBParameterValues.ApiKey,
+            Constants.TMDBParameterKeys.SessionID : self.appDelegate.sessionID
+        ]
+            
+        let header = ["content-type": "application/json;charset=utf-8"]
+        
+        let postParameters = [
+            "media_type": "movie",
+            "media_id": movie!.id,
+            "favorite": shouldFavorite
+        ]
+        
         /* 2/3. Build the URL, Configure the request */
-        /* 4. Make the request */
-        /* 5. Parse the data */
-        /* 6. Use the data! */
-        /* 7. Start the request */
-        
-        /* If the favorite/unfavorite request completes, then use this code to update the UI...
-        
-        performUIUpdatesOnMain {
-            self.favoriteButton.tintColor = (shouldFavorite) ? nil : UIColor.blackColor()
+        let request = NSMutableURLRequest(URL: appDelegate.tmdbURLFromParameters(methodParameters, withPathExtension: "/account/" + String(appDelegate.userID) + "/favorite"))
+        if NSJSONSerialization.isValidJSONObject(postParameters) {
+            do {
+                let postData = try NSJSONSerialization.dataWithJSONObject(postParameters, options: .PrettyPrinted)
+                request.HTTPBody = postData
+            } catch {
+                print("Error creating JSON POST data")
+            }
         }
+
+        request.HTTPMethod = "POST"
+        request.allHTTPHeaderFields = header
+
+        let task = appDelegate.sharedSession.dataTaskWithRequest(request) { (data, response, error) in
+            /* 5. Parse the data */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                print("Your request returned a status code other than 2xx!")
+                return
+            }
+
+            
+//            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode == 1 || statusCode == 12 || statusCode == 13 else {
+//                print("Your request returned a status code other than 1, 12, 13")
+//                return
+//            }
         
-        */
+        /* 6. Use the data! */
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 5. Parse the data */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did we receive a TMDB status_code? */
+            guard let tmdbStatusCode = parsedResult[Constants.TMDBResponseKeys.StatusCode] as? Int else {
+                print("Could not find key '\(Constants.TMDBResponseKeys.StatusCode)' in  \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Did we receive the correct TMDB status_code? */
+            if shouldFavorite && !(tmdbStatusCode == 12 || tmdbStatusCode == 1) {
+                print("Unrecognized '\(Constants.TMDBResponseKeys.StatusCode)' in  \(parsedResult)")
+                return
+            } else if !shouldFavorite && tmdbStatusCode != 13 {
+                print("Unrecognized '\(Constants.TMDBResponseKeys.StatusCode)' in  \(parsedResult)")
+                return
+            }
+
+            self.isFavorite = shouldFavorite
+
+            performUIUpdatesOnMain {
+                self.favoriteButton.tintColor = (shouldFavorite) ? nil : UIColor.blackColor()
+            }
+        }
+        task.resume()
     }
 }
